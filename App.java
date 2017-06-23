80989aa455160285310047e937ec7e421b486f01
Byron Blocker <byronblocker@gmail.com>
2017-06-22 21:20:50 -0700

    begin calculator project

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+  public static void main(String[] args) {
+    Calc calculator = new Calc();
+    int addTest = calculator.add(3, 5);
+    System.out.println(addTest);
+
+    int subtractTest = calculator.subtract(7, 4);
+    System.out.println(subtractTest);
+
+
+  }
+}
---
+++ Calc.java
@@ -0,0 +1,15 @@
+public class Calc {
+  int firstInput;
+  int secondInput;
+
+  public int add(int a, int b){
+    int addTotal = a + b;
+    return addTotal;
+  }
+
+  public int subtract(int a, int b) {
+    int subtractTotal = a - b;
+    return subtractTotal;
+  }
+
+}
